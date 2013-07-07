e2b5b5dacab2dfb8911fbe4fd5779ac3065c6f6d
Marcello Righelli <mrighelli@stanford.edu>
2013-07-07 17:09:47 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync "index.html");
-  var buffer = new Buffer(data);	
-  response.send(buffer.toString());
+  var buffer = new Buffer(data);	   
+response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
